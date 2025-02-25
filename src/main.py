6fa4f2188eff348163c79ac0ea3e56238d4f8f73
Andreas Loizides <andreas16700@icloud.com>
2025-02-25 05:10:04 +0200

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,39 +1,30 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
-import os
+import os, speedtest
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
+    if context.req.query and 'results' in context.req.query:
+        context.log(f"measuring..")
+        st = speedtest.Speedtest(secure=True)
+        st.get_best_server()
+        download_speed = st.download() / 1e6  # Convert from bits/s to Mbps
+        upload_speed = st.upload() / 1e6  # Convert from bits/s to Mbps
+        context.log(f"measured download speed: {download_speed} upload speed: {upload_speed}")
+        return context.res.text(f"""<div id="speed-test-results">
+      <div class="speed">
+        <div>
+          <h2>{download_speed:.2f} Mbps</h2>
+          <p>Download Speed</p>
+        </div>
+        <div>
+          <h2>{upload_speed:.2f} Mbps</h2>
+          <p>Upload Speed</p>
+        </div>
+      </div>
+    </div>
+    """, 200, {"content-type": "text/html"})
 
-    try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    with open("speedtest.html", "r", encoding="utf-8") as file:
+        html_text = file.read()
+        return context.res.text(html_text, 200, {"content-type": "text/html"})
